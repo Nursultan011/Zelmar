f2cdb3244edc81269b6daab8684d83879a043c66
Zelmar <example@gamil.com>
2020-05-18 00:55:43 +0600

    Pages-header done

--- src/js/script.js
+++ src/js/script.js
@@ -1,4 +1,20 @@
 $(document).ready(function(){
 
     $('input[name=phone').mask("+7 (999) 999-99-99");
+
+    $('.hamburger').on('click', function(e) {
+        e.preventDefault;
+        $(this).toggleClass('hamburger_active');
+        return;
+    });
+
+    $('.hamburger').click(function(){
+        if ($(".active__menu").is(":visible") == true) {
+            $('.active__menu').hide();
+            $('.header').removeClass("slick_active")
+        } else {
+            $('.active__menu').show();
+            $(".header").addClass("slick_active")
+        }
+    });
 });
