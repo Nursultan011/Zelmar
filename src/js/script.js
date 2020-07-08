053090663457418c28447879e4e3ebca8ca28400
Zelmar <example@gamil.com>
2020-07-08 14:58:04 +0600

    slider

--- src/js/script.js
+++ src/js/script.js
@@ -1,14 +1,14 @@
-$(document).ready(function(){
+$(document).ready(function () {
 
     $('input[name=phone').mask("+7 (999) 999-99-99");
 
-    $('.hamburger').on('click', function(e) {
+    $('.hamburger').on('click', function (e) {
         e.preventDefault;
         $(this).toggleClass('hamburger_active');
         return;
     });
 
-    $('.hamburger').click(function(){
+    $('.hamburger').click(function () {
         if ($(".active__menu").is(":visible") == true) {
             $('.active__menu').hide();
             $('.header').removeClass("slick_active")
@@ -18,7 +18,7 @@ $(document).ready(function(){
         }
     });
 
-    $('.close_navbar').on('click', function(){
+    $('.close_navbar').on('click', function () {
         $('.active__menu').fadeOut();
     });
 
@@ -29,18 +29,48 @@ $(document).ready(function(){
         return false;
     });
 
-    $('[data-modal=concrete').on('click', function(){
-        $('.product-range__concrete').css("color","#EFAA00").css("border-bottom","3px solid #EFAA00");
+    $('[data-modal=concrete').on('click', function () {
+        $('.product-range__concrete').css("color", "#EFAA00").css("border-bottom", "3px solid #EFAA00");
         $('#Concrete').fadeIn();
         $('#Block').fadeOut();
-        $('.product-range__block').css("color","#0F0E0F").css("border-bottom","0px");
+        $('.product-range__block').css("color", "#0F0E0F").css("border-bottom", "0px");
     });
 
-    $('[data-modal=block').on('click', function(){
-        $('.product-range__block').css("color","#EFAA00").css("border-bottom","3px solid #EFAA00");;
+    $('[data-modal=block').on('click', function () {
+        $('.product-range__block').css("color", "#EFAA00").css("border-bottom", "3px solid #EFAA00");;
         $('#Block').fadeIn();
         $('#Concrete').fadeOut();
-        $('.product-range__concrete').css("color","#0F0E0F").css("border-bottom","0px");
+        $('.product-range__concrete').css("color", "#0F0E0F").css("border-bottom", "0px");
+    });
+
+    $('.last-news__slider').slick({
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        prevArrow: '<img class="last-news__slider-arrow last-news__slider-arrow-left" src="img/arrow-left.svg" alt="arrows-left">',
+        nextArrow: '<img class="last-news__slider-arrow last-news__slider-arrow-right" src="img/arrow-right.svg" alt="arrows-right">',
+        responsive: [
+            {
+                breakpoint: 1199,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1,
+                }
+            }, {
+                breakpoint: 991,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                }
+            }, {
+                breakpoint: 767,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                    prevArrow: false,
+                    nextArrow: false,
+                }
+            }
+        ]
     });
 });
 
